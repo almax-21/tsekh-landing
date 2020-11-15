8daa491662de466836271dbc4b2bd0bb84db7098
almax-21 <almaev21.max@gmail.com>
2020-11-16 02:02:18 +0400

    Add scroll-up button functionality

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,5 @@
+// === nav-button ===
+
 const header = document.querySelector(".header");
 const navMain = header.querySelector(".main-nav");
 const navToggle = header.querySelector(".toggle");
@@ -16,3 +18,43 @@ navToggle.addEventListener("click", function () {
     navToggle.classList.remove("toggle--active");
   }
 });
+
+// === / nav-button ===
+
+// === scroll-up ===
+
+const offset = 100;
+const scrollUp = document.querySelector('.scroll-up');
+const scrollUpSvgPath = document.querySelector('.scroll-up__svg-path');
+const pathLength = scrollUpSvgPath.getTotalLength();
+
+scrollUpSvgPath.style.strokeDasharray = `${pathLength} ${pathLength}`;
+scrollUpSvgPath.style.transition = 'stroke-dashoffset 20ms';
+
+const getTop = () => window.pageYOffset || document.documentElement.scrollTop;
+
+const updateDashOffset = () => {
+  const height = document.documentElement.scrollHeight - window.innerHeight;
+  const dashoffset = pathLength - ((getTop() * pathLength) / height);
+
+  scrollUpSvgPath.style.strokeDashoffset = dashoffset;
+};
+
+window.addEventListener('scroll', () => {
+  updateDashOffset();
+
+  if (getTop() > offset) {
+    scrollUp.classList.add('scroll-up--active');
+  } else {
+    scrollUp.classList.remove('scroll-up--active');
+  }
+});
+
+scrollUp.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
+
+// === / scroll-up ===
